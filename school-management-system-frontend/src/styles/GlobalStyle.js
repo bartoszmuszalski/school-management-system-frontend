ab0f1fb359bf60f61afcacb4df0636ae2fcf26f6
bartoszmuszalski <bartoszmuszalski@wp.pl>
2025-01-10 15:37:51 +0100

    Change of Login

--- school-management-system-frontend/src/styles/GlobalStyle.js
+++ school-management-system-frontend/src/styles/GlobalStyle.js
@@ -4,11 +4,7 @@ const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    background: linear-gradient(
-      to bottom right,
-      rgb(106, 156, 119),
-      rgb(64, 15, 187)
-    );
+    background-color: white; /* Changed from gradient to white */
     font-family: 'Roboto', sans-serif;
     min-height: 100vh;
   }
